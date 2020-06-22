91e1a5c74161698f9cc0de01cc574316f6c0a8ae
Wil-g2 <wil-g2@hotmail.com>
2020-06-21 21:45:07 -0300

    feat: added totalIten e cartTotal

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -25,14 +25,16 @@ const FloatingCart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    const total = products.reduce(
+      (accum, product) => accum + product.price * product.quantity,
+      0,
+    );
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    return products.reduce((accum, product) => accum + product.quantity, 0);
   }, [products]);
 
   return (
